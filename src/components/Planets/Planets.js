e553e7f5c55adf7c3b1599d4809be2eb6c852d73
Gastón Benitez <gaston.benitez@corp.smtsoftware.com>
2021-11-02 16:37:44 -0300

    fixes

--- src/components/Planets/Planets.js
+++ src/components/Planets/Planets.js
@@ -2,8 +2,7 @@ import React from 'react';
 import Box from '../Box/Box';
 
 const Planets = (data) => {
-  // console.log(data);
-  console.log(data.planetData.rotation)
+  
   return (
     <div>
       <h1>planets component</h1>
