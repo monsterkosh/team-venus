92025c597e7d061ca53af5420a67a5791f69454f
monsterkosh <mnnnkosh@gmail.com>
2021-11-04 02:18:40 -0300

    hero comp views

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,14 +1,12 @@
 import style from './Box.module.css';
 
-
-const Box = ({title, data}) => {
-
-    return (
-            <div className={style.footer_boxes}>
-                <h2>{title}</h2>
-                <h4>{data}</h4>
-            </div>
-    )
-}
+const Box = ({ title, data }) => {
+  return (
+    <div className={style.footer_boxes}>
+      <h2>{title}</h2>
+      <h4>{data}</h4>
+    </div>
+  );
+};
 
 export default Box;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import style from './Home.module.css';
 import Box from '../Box/Box';
+import arrowImg from '../../assets/arrow.svg';
 
 const Home = (data) => {
   let overview = {
@@ -54,30 +55,61 @@ const Home = (data) => {
               surface
             </button>
           </div>
-          <div className={style.hero_images}>
-            <img src={planetData.img} alt='' />
-          </div>
-          {planetData.geo ? (
-            <div className={style.hero_images__mini}>
-              <img src={planetData.geo} alt='' />
+          <div className={style.hero_left}>
+            <div className={style.hero_images}>
+              <img src={planetData.img} alt='' />
             </div>
-          ) : (
-            false
-          )}
-          <div className={style.hero_info}>
-            <div className={style.hero_title}>{planetData.title}</div>
-            <div className={style.hero_info__desc}>{planetData.desc}</div>
-            <div className={style.hero_info__wiki}>
-              Source: <a href={planetData.wiki}>Wikipedia</a>
+            {planetData.geo ? (
+              <div className={style.hero_images__mini}>
+                <img src={planetData.geo} alt='' />
+              </div>
+            ) : (
+              false
+            )}
+          </div>
+          <div className={style.hero_right}>
+            <div className={style.hero_info}>
+              <div className={style.hero_title}>{planetData.title}</div>
+              <div className={style.hero_info__desc}>{planetData.desc}</div>
+              <div className={style.hero_info__wiki}>
+                Source:{' '}
+                <a href={planetData.wiki}>
+                  Wikipedia
+                  <img src={arrowImg} alt='go to link'></img>
+                </a>
+              </div>
+            </div>
+            <div className={style.hero_views_desk}>
+              <button
+                onClick={() => {
+                  setPlanetData(overview);
+                }}
+              >
+                overwiew
+              </button>
+              <button
+                onClick={() => {
+                  setPlanetData(structure);
+                }}
+              >
+                structure
+              </button>
+              <button
+                onClick={() => {
+                  setPlanetData(surface);
+                }}
+              >
+                surface
+              </button>
             </div>
           </div>
         </div>
-      </div>
-      <div className={style.boxes_container}>
-        <Box title='rotation time' data={data.planetData.rotation} />
-        <Box title='revolution time' data={data.planetData.revolution} />
-        <Box title='radius' data={data.planetData.radius} />
-        <Box title='average temp' data={data.planetData.temperature} />
+        <div className={style.boxes_container}>
+          <Box title='rotation time' data={data.planetData.rotation} />
+          <Box title='revolution time' data={data.planetData.revolution} />
+          <Box title='radius' data={data.planetData.radius} />
+          <Box title='average temp' data={data.planetData.temperature} />
+        </div>
       </div>
     </div>
   );
